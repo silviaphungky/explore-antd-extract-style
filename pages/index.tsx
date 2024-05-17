92fece89f82f89cea936e26bae0567ce64e41ca4
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-05-17 09:15:15 +0700

    interval

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,19 @@ export default function Home() {
   const [data, setData] = useState<Array<number>>([])
 
   useEffect(() => {
-    Array.from(Array(100000).keys()).forEach((el) => {
-      setData((prev) => [el, ...prev].slice(0, 19))
-    })
+    const interval = setInterval(() => {
+      setData((prev) => {
+        return [(prev[0] || 0) + 1, ...prev].slice(0, 19)
+      })
+    }, 5)
+
+    return () => clearInterval(interval)
   }, [])
+
   return (
     <>
       {data.map((item) => (
         <Flex key={item}>
-          <Typography>{item}</Typography>
-          <Typography>{item}</Typography>
-          <Typography>{item}</Typography>
-          <Typography>{item}</Typography>
-          <Typography>{item}</Typography>
-          <Typography>{item}</Typography>
           <Typography>{item}</Typography>
         </Flex>
       ))}
